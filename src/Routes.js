28c4a79bad41eb0ef4c922bba1b5177baa1e6cf0
Eamon Dang <eamondang@gmail.com>
2023-05-14 17:15:13 +0700

    fix: app route

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"
+import App from "./App"
 import TicketElite from "./components/TicketElite"
 import TicketPremium from "./components/TicketPremium"
 import TicketStandard from "./components/TicketStandard"
@@ -6,6 +7,7 @@ import TicketStandard from "./components/TicketStandard"
 const Routers = () => {
   return (
     <Routes>
+      <Route path="/" element={<App />}></Route>
       <Route path="premium" element={<TicketPremium />}></Route>
       <Route path="elite" element={<TicketElite />}></Route>
       <Route path="standard" element={<TicketStandard />}></Route>
